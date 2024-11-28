5fa4b5264a411a17b9d4017ed65b50991e4a697b
GuillermoHuamanM <108649901+GuillermoHM@users.noreply.github.com>
2024-11-28 10:24:35 -0500

    Delete nodemongo directory

--- nodemongo/app/models/db.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-// Estableciendo conexión con MongoDB
-mongoose.connect('mongodb://localhost:27017/ucsm')
-  .then(() => console.log('Conexión exitosa con MongoDB'))
-  .catch((err) => console.error('Error al conectar a MongoDB:', err));
-
-module.exports = mongoose;
